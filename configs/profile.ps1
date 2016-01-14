ed249e4f43d3ddd0254489a93d920bba65dbe8bd
Chet Husk <chet_husk@volusion.com>
2016-01-14 16:45:25 -0600

    more changes

--- configs/profile.ps1
+++ configs/profile.ps1
@@ -1,39 +1,62 @@
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
-
 # ALIASES
-Set-Alias sublime "C:\Program Files\Sublime Text 3\sublime_text.exe"
+Set-Alias subl "C:\Program Files\Sublime Text 3\sublime_text.exe"
 Set-Alias g git
-Set-Alias many git
-Set-Alias much git
-Set-Alias such git
+Set-Alias fsi "C:\Program Files (x86)\Microsoft SDKs\F#\4.0\Framework\v4.0\Fsi.exe"
 
 function which($name)
 {
     Get-Command $name | Select-Object -ExpandProperty Definition
 }
 
+function deploy(){
+	param($project, $version, [string[]]$envs)
+	foreach($env in $envs){
+		octo deploy-release --apikey=$env:OctoAPI --server=http://mozupus.corp.volusion.com --project=$project --version=$version --deployTo=$env
+	}
+}
+
+function tfbuild(){
+	param(
+		$server = $env:TFSServer,
+		$collection = "vNext",
+		$project = "v2Mozu",
+		$build, 
+		$branch="master", 
+		[switch]$stable)
+& 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\tfsbuild.exe' start $server/$collection $project $build /msBuildArguments:"/p:BranchOverride=$branch;Stable=$stable;"
+}
+function update(){
+    param([string[]]$ids)
+    if(Test-Path .nuget){
+        $sln = get-childitem "*.sln"
+        nuget restore $sln
+        foreach($id in $ids){
+            nuget update $sln -Id $id
+        }
+    }
+    else{
+        .paket\paket.bootstrapper.exe
+        foreach($id in $ids){
+            .paket\paket update nuget $id
+        }
+    }
+}
 # END ALIASES
-
-
 # PATH
 $env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\bin;"
 $env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\usr\bin;"
 $env:path += ";C:\code\global_configs\scripts;"
 $env:path += ";$env:MONGOPATH;"
 # END PATH
-
-
 #GIT STUFF
 # MODULES
 if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
-. 'C:\tools\poshgit\dahlbyk-posh-git-7acc70b\profile.example.ps1'
-
+. 'C:\tools\poshgit\dahlbyk-posh-git-fadc4dd\profile.example.ps1'
 # customize colors for git since the blue background makes red hard to see
 # these set the prompt colors
 $GitPromptSettings.WorkingForegroundColor = [ConsoleColor]::Red
-$GitPromptSettings.UntrackedForeGroundColor = [ConsoleColor]::Red
 #END GIT STUFF
-
 # Set up a simple prompt, adding the git prompt parts inside git repos
 function global:prompt {
     $realLASTEXITCODE = $LASTEXITCODE
